34ca7bc713621f20f0f01f7f3d4106d0a9a84c53
Juan Jose Garcia <shizan@hotmail.com>
2016-02-26 21:41:21 +0100

    Mandar Mensaje Queue

---
+++ MandarMensajeQueue/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Queue;
+using Newtonsoft.Json;
+
+namespace MandarMensajeQueue
+{
+    class Program
+    {
+        public class Message
+        {
+            public string Asunto { get; set; }
+            public string Contenido { get; set; }
+
+        }
+
+        static void Main(string[] args)
+        {
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(ConfigurationManager.ConnectionStrings["ConexionAzure"].ConnectionString);
+            CloudQueueClient queueClient = storageAccount.CreateCloudQueueClient();
+
+            CloudQueue queue = queueClient.GetQueueReference("messages");
+            queue.CreateIfNotExists();
+
+            Console.WriteLine("Enviar mensajes.... (ESC para sali)");
+
+            Message mess = new Message();
+
+            do
+            {
+                Console.Write("Asunto: ");
+                mess.Asunto = Console.ReadLine();
+                Console.Write("Contenido: ");
+                mess.Contenido = Console.ReadLine();
+
+                queue.AddMessage(
+                        new CloudQueueMessage(JsonConvert.SerializeObject(mess)),
+                        TimeSpan.FromDays(5.0),
+                        TimeSpan.FromDays(0.0)
+                    );
+
+
+            } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
+
+
+        }
+    }
+}
